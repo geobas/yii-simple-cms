9730df611edba91bae71fc422d77383a15df8236
ksenera <ksenera@yahoo.com>
2015-07-14 18:32:08 +0300

    Create Article model / Category model / CRUD for Article

---
+++ yii-cms/protected/models/Article.php
@@ -0,0 +1,139 @@
+<?php
+
+/**
+ * This is the model class for table "tbl_article".
+ *
+ * The followings are the available columns in table 'tbl_article':
+ * @property string $id
+ * @property string $title
+ * @property string $body
+ * @property string $image
+ * @property integer $published
+ * @property string $create_time
+ * @property string $update_time
+ *
+ * The followings are the available model relations:
+ * @property Category[] $categories
+ */
+class Article extends CActiveRecord
+{
+	/**
+	 * @var private property containing the associated Category model instance.
+	 */
+	private $category = null;
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'tbl_article';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('title, category_id, body, image, published, create_time, update_time', 'required'),
+			array('published', 'numerical', 'integerOnly'=>true),
+			array('title', 'length', 'max'=>255),
+			array('image', 'length', 'max'=>150),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, title, body, image, published, create_time, update_time', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'categories' => array(self::MANY_MANY, 'Category', 'tbl_article_category(article_id, category_id)'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'title' => 'Title',
+			'body' => 'Body',
+			'image' => 'Image',
+			'published' => 'Published',
+			'create_time' => 'Create Time',
+			'update_time' => 'Update Time',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id,true);
+		$criteria->compare('title',$this->title,true);
+		$criteria->compare('body',$this->body,true);
+		$criteria->compare('image',$this->image,true);
+		$criteria->compare('published',$this->published);
+		$criteria->compare('create_time',$this->create_time,true);
+		$criteria->compare('update_time',$this->update_time,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Article the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * Returns all article categories
+	 * @return array array of categories
+	 */
+	public function getCategoryOptions()
+	{
+		// $criteria = new CDbCriteria();
+		// $criteria->select = 'title';
+		$categories = Category::model()->findAll(array('select' => 'id, title'));
+
+		$options = array();
+		foreach ( $categories as $category )
+		{
+			$options[$category['id']] = $category['title'];
+		}
+
+		return $options;
+	}
+}
---
+++ yii-cms/protected/views/article/_form.php
@@ -0,0 +1,70 @@
+<?php
+/* @var $this ArticleController */
+/* @var $model Article */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'article-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'title'); ?>
+		<?php echo $form->textField($model,'title',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'title'); ?>
+	</div>
+
+<!-- 	<div class="row">
+		<?php //echo $form->labelEx($model,'category'); ?>
+		<?php //echo $form->dropDownList($model,'category', $model->getCategoryOptions()); ?>
+		<?php //echo $form->error($model,'category'); ?>
+	</div>	
+ -->
+	<div class="row">
+		<?php echo $form->labelEx($model,'body'); ?>
+		<?php echo $form->textArea($model,'body',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->error($model,'body'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'image'); ?>
+		<?php echo $form->textField($model,'image',array('size'=>60,'maxlength'=>150)); ?>
+		<?php echo $form->error($model,'image'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'published'); ?>
+		<?php echo $form->textField($model,'published'); ?>
+		<?php echo $form->error($model,'published'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'create_time'); ?>
+		<?php echo $form->textField($model,'create_time'); ?>
+		<?php echo $form->error($model,'create_time'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'update_time'); ?>
+		<?php echo $form->textField($model,'update_time'); ?>
+		<?php echo $form->error($model,'update_time'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
