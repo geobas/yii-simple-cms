05b5fb5ec3517d7a1d2c4b143955795f72580a97
ksenera <ksenera@yahoo.com>
2015-07-22 18:11:55 +0300

    Show the last time a user logged in

--- yii-cms/protected/views/site/index.php
+++ yii-cms/protected/views/site/index.php
@@ -6,6 +6,8 @@ $this->pageTitle=Yii::app()->name;
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
+<p><?php if ( isset(Yii::app()->user->lastLoginTime) ) echo 'You last logged in on ' . date( 'l, F d, Y, g:i a', Yii::app()->user->lastLoginTime); ?></p>
+
 <p>Congratulations! You have successfully created your Yii application.</p>
 
 <p>You may change the content of this page by modifying the following two files:</p>
