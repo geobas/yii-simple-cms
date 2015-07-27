b3f49fae9acc1e2df4f0942947aa42cbc6901356
ksenera <ksenera@yahoo.com>
2015-07-27 17:22:59 +0300

    Disable Gii password

--- yii-cms/protected/config/main.php
+++ yii-cms/protected/config/main.php
@@ -32,7 +32,7 @@ return array(
 		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'admin',
+			'password'=>false,
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 			'generatorPaths' => array('vendor.crisu83.yiistrap.gii'),
