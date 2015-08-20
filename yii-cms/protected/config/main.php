66ea52252beecb0c5aa9f31627d4a0733d46bea3
ksenera <ksenera@yahoo.com>
2015-08-20 20:27:38 +0300

    Add custom error views for debug & production mode

--- yii-cms/protected/config/main.php
+++ yii-cms/protected/config/main.php
@@ -84,7 +84,7 @@ return array(
 
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
-			'errorAction'=>'site/error',
+			// 'errorAction'=>'site/error',
 		),
 
 		'log'=>array(
