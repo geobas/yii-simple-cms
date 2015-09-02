b56c2b39c292f626ce2b6189134a6c3b05fcc8ed
ksenera <ksenera@yahoo.com>
2015-09-02 16:59:40 +0300

    Refactor language selector widget

--- yii-cms/protected/components/widgets/LanguageSelector.php
+++ yii-cms/protected/components/widgets/LanguageSelector.php
@@ -4,14 +4,20 @@
  */
 class LanguageSelector extends CWidget
 {
+	private $_currentLang;
+	private $_languages;
+
+	public function init()
+	{
+		$this->_currentLang = Yii::app()->language;
+		$this->_languages = Yii::app()->params->languages;
+	}
+
     public function run()
-    {
-        $currentLang = Yii::app()->language;
-        $languages = Yii::app()->params->languages;
-        
+    {        
         $this->render('languageSelector', array(
-        									'currentLang' => $currentLang,
-        									'languages'=>$languages,
+        									'currentLang' => $this->_currentLang,
+        									'languages'=>$this->_languages,
         								));
     }
 }
