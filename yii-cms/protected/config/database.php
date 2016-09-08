a4854381fe0335dee29a8a98350210f3fb49c588
geobas <ksenera@yahoo.com>
2016-09-08 23:00:36 +0300

    Add heroku's db creds

--- yii-cms/protected/config/database.php
+++ yii-cms/protected/config/database.php
@@ -3,11 +3,11 @@
 // This is the database connection configuration.
 return array(
 	// 'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-	// uncomment the following lines to use a MySQL database	
-	'connectionString' => 'mysql:host=localhost;dbname=CMS',
+	// uncomment the following lines to use a MySQL database
+	'connectionString' => ( getenv('HOST') ) ? getenv('HOST') : 'mysql:host=localhost;dbname=CMS',
 	'emulatePrepare' => true,
 	'schemaCachingDuration' => 86400, // one day
-	'username' => 'root',
-	'password' => 'root',
-	'charset' => 'utf8',	
+	'username' => ( getenv('USERNAME') ) ? getenv('USERNAME') : 'root',
+	'password' => ( getenv('PASSWORD') ) ? getenv('PASSWORD') : 'root',
+	'charset' => 'utf8',
 );
