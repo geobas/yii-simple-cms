d229755eb6a17d0c3098254be16fd09fe40502db
George Basteas <ksenera@yahoo.com>
2016-09-09 15:23:18 +0300

    Debugging heroku's db connection #7

--- yii-cms/protected/config/database.php
+++ yii-cms/protected/config/database.php
@@ -4,10 +4,10 @@
 return array(
 	// 'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	'connectionString' => ( apache_getenv("HOST") ) ? 'mysql:host=' . apache_getenv('HOST') . ';dbname=' . apache_getenv('DATABASE') : 'mysql:host=localhost;dbname=CMS',
+	'connectionString' => ( $_ENV("HOST") ) ? 'mysql:host=' . $_ENV('HOST') . ';dbname=' . $_ENV('DATABASE') : 'mysql:host=localhost;dbname=CMS',
 	'emulatePrepare' => true,
 	'schemaCachingDuration' => 86400, // one day
-	'username' => ( apache_getenv('USERNAME') ) ? apache_getenv('USERNAME') : 'root',
-	'password' => ( apache_getenv('PASSWORD') ) ? apache_getenv('PASSWORD') : 'root',
+	'username' => ( $_ENV('USERNAME') ) ? $_ENV('USERNAME') : 'root',
+	'password' => ( $_ENV('PASSWORD') ) ? $_ENV('PASSWORD') : 'root',
 	'charset' => 'utf8',
 );
